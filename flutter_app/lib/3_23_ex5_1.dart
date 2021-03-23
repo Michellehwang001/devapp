7cefaba4c4f3137e4a9306a8287f56552f6b14c8
Michelle Hwang <jimin_ma@naver.com>
2021-03-23 16:50:34 +0900

    homework 5-1

---
+++ flutter_app/lib/3_23_ex5_1.dart
@@ -0,0 +1,15 @@
+void main() {
+  introduceOneself();
+}
+
+void introduceOneself() {
+  String name = '황혜정';
+  int age = 20;
+  double height = 170;
+  String gender = '소녀';
+
+  print('안녕하세요. $name 이라고 합나다.\n항상 $age살 같은 열정으로 $height cm의 자신감으로 살아가고 있습니다.\n');
+  print('예쁜 7살 $gender같은 딸도 있고, 곧 사춘기가 다가오는 아들내미 한명도 있습니다. ');
+  print('아참! 가끔 존재를 잊어버리는 자신감 뿜뿜 남편님도 있습니다.');
+  print('플러터를 배우고 있는 요즘 참 행복합니다. 열심히 공부해서 내가 만든 앱을 전세계 사람들이 사용하는게 새로 생긴 목표입니다.\n열심히 하겠습니다. ^^');
+}
