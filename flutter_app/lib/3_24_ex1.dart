6db94867deb46ab0996dfe73a24b516c541fb36a
Michelle Hwang <jimin_ma@naver.com>
2021-03-24 16:05:48 +0900

    Modify 3_24_ex1.dart

--- flutter_app/lib/3_24_ex1.dart
+++ flutter_app/lib/3_24_ex1.dart
@@ -4,10 +4,11 @@ void main(){
   List<int> numbers = [3, 4, 5, 10];
   print('1자의 숫자를 입력해 주세요');
 
-  String inputNum = stdin.readLineSync();
+  String inputString = stdin.readLineSync();
+  int inputNum = int.parse(inputString);
 
   for(var number in numbers) {
-    if(number == int.parse(inputNum)) {
+    if(number == inputNum) {
       print('정답!!!');
       break;
     }
