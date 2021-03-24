97f60bb3597d34f9280d3ce0f703bed793f65664
Michelle Hwang <jimin_ma@naver.com>
2021-03-24 10:06:56 +0900

    연습문제 수정

--- flutter_app/lib/3_23_ex5_4.dart
+++ flutter_app/lib/3_23_ex5_4.dart
@@ -1,11 +1,12 @@
 import 'dart:math';
 
-// 삼각형 면적,  원의 면적
 void main() {
   print ('밑변이 10, 높이가 5인 삼각형의 넓이는? ${calcTriangleArea(10, 5)} 입니다.');
   print ('반지름이 10인 원의 넓이는? ${calcCircleArea(10)} 입니다.');
 }
 
+// 삼각형 면적 계산
 double calcTriangleArea(double bottom, double height) => bottom * height * 0.5;
 
+// 원의 면적 계산
 double calcCircleArea(double radius) => pi * pow(radius, 2);
