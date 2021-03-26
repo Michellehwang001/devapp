a48bbe4cc63645568ac819a65ccfc4bf726f73b5
Hwang Hyejung <jimin_ma@naver.com>
2021-03-26 12:39:11 +0900

    2021/3/26 static2

--- flutter_app/lib/3_26.dart
+++ flutter_app/lib/3_26.dart
@@ -5,6 +5,7 @@ import 'package:flutter_app/equipment/sword.dart';
 void main() {
   // 생성자를 통한 초기화
   // 더 이상 이름, hp설정을 할 수 없다.
+  // 추가
   // 버그를 줄이기 위해서, 사람이 실수하는 것을 막기 위해 private 변수 사용
   Hero hero = Hero('Michelle', 50);
   Sword sword = Sword('불의검', 10, 1000, '착용자에게 불데미지 +10');
