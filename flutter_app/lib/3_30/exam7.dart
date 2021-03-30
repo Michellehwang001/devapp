33661d02902f0cab4b98aec77f7e0d8d7793c6ad
Hwang Hyejung <jimin_ma@naver.com>
2021-03-30 12:32:27 +0900

    3/30 Modify exam7

--- flutter_app/lib/3_30/exam7.dart
+++ flutter_app/lib/3_30/exam7.dart
@@ -1,24 +1,21 @@
 // 두 정수를 입력받아 비교하여 더 큰 수를 출력하는 프로그램
 // 두 수가 같은 경우 eq를 출력
 
+import 'dart:math';
+
 void main() {
   Exam exam = Exam();
-  int answer = exam.solution(20, 20);
-  if (answer == 0) {
-    print('eq');
-  } else {
-    print(answer);
-  }
+  print(exam.solution(20, 30));
+  print(exam.solution(20, 10));
+  print(exam.solution(20, 20));
 }
 
 class Exam {
-  int solution(int a, int b) {
-    int result = 0;
+  String solution(int a, int b) {
+    String result = 'eq';
 
-    if (a > b) {
-      result = a;
-    } else if (a < b) {
-      result = b;
+    if(a != b) {
+      result = max(a, b).toString();
     }
 
     return result;
