564ada086413330518bb65b43ba6a1912f2474bf
Hwang Hyejung <jimin_ma@naver.com>
2021-03-30 10:41:09 +0900

    3/30 Modify exam

--- flutter_app/lib/3_30/exam5.dart
+++ flutter_app/lib/3_30/exam5.dart
@@ -1,9 +1,13 @@
 // 주사위의 한쪽면과 반대쪽 숫자의 합은 반드시 7이 된다. (1, 6) (2, 5) (3, 4)
 // 한쪽면이 n일 경우 반대쪽 숫자는?
+import 'dart:math';
+
 void main() {
   Exam exam = Exam();
-  print(exam.solution(3));
-  print(exam.solution(1));
+  Random rnd = Random();
+
+  print(exam.solution(rnd.nextInt(6)+1));
+  print(exam.solution(rnd.nextInt(6)+1));
 }
 
 class Exam {
