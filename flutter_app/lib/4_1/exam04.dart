d6f9f4b5a93482fbbabf13988dae2e8c72d906b8
Hwang Hyejung <jimin_ma@naver.com>
2021-04-01 14:31:20 +0900

    4월1일 연습문제4

---
+++ flutter_app/lib/4_1/exam04.dart
@@ -0,0 +1,29 @@
+// 이름을 영어로 입력하면 성과 이름의 각 첫 문자를 . 으로 연결해서 출력하시오
+import 'dart:io';
+
+void main() {
+  // String input = stdin.readLineSync();
+  List<String> inputLine = stdin.readLineSync().split(' ');
+  Exam exam = Exam(inputLine);
+  print (exam.shorten());
+
+}
+
+class Exam {
+  List<String> inputLine;
+
+  Exam(this.inputLine);
+
+  String shorten() {
+    String result = '';
+
+    for (int i = 0; i < inputLine.length; i++) {
+      result += inputLine[i][0];
+      if (i != inputLine.length - 1){
+        result += '.';
+      }
+    }
+
+    return result;
+  }
+}
