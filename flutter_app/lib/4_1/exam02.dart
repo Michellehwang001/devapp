8ef673566c98301d0aec50c4ffecc99f595a0dd8
Hwang Hyejung <jimin_ma@naver.com>
2021-04-01 15:12:19 +0900

    4월1일 연습문제2 알파벳 A 갯수 구하기

---
+++ flutter_app/lib/4_1/exam02.dart
@@ -0,0 +1,25 @@
+// 문자열 s가 주어졌을 때 s에 포함된 알파벳 'A'의 갯수를 구하시오
+import 'dart:io';
+
+void main() {
+  String s = stdin.readLineSync();
+  Exam exam = Exam(s);
+  print(exam.solution());
+}
+
+class Exam {
+  String s;
+
+  Exam(this.s);
+
+  int solution() {
+    int result = 0;
+    for (int i = 0; i < s.length; i++) {
+      if (s[i].toUpperCase() == 'A') {
+        result++;
+      }
+    }
+
+    return result;
+  }
+}
