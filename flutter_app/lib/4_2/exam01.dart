c3247acb0910018eaef18e1bf85017a59909baa2
Hwang Hyejung <jimin_ma@naver.com>
2021-04-02 09:39:08 +0900

    4월1일 연습문제5 두수의 차

---
+++ flutter_app/lib/4_2/exam01.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+  //List<int> inputLine = stdin.readLineSync().split(' ').map((e) => int.parse(e));
+
+  List<String> inputLine = stdin.readLineSync().split(' ');
+  int N = int.parse(inputLine[0]);
+  int M = int.parse(inputLine[1]);
+
+  Exam exam = Exam(N, M);
+  print(exam.solution());
+}
+
+class Exam {
+  int N, M;
+
+  Exam(this.N, this.M);
+
+  String solution() {
+    String result = 'No';
+
+    if ((N % 2 == 0 && M % 2 == 1) || (N % 2 == 1 && M % 2 == 0)) {
+      result = 'Yes';
+    }
+
+    return result;
+  }
+}
