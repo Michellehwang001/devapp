ac527ab39a8f9eec01beb74b486e01002282120f
Hwang Hyejung <jimin_ma@naver.com>
2021-04-15 06:25:05 +0900

    미세먼지앱 시작

---
+++ flutter_dust/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
