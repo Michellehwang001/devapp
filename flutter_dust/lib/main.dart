17e5d5e0d8cc0e09c5b4deb9e870299e287b8a1b
Hwang Hyejung <jimin_ma@naver.com>
2021-04-15 12:25:10 +0900

    app 웹에서 실행

--- flutter_dust/lib/main.dart
+++ flutter_dust/lib/main.dart
@@ -27,16 +27,54 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('미세먼지앱'),
+      appBar: AppBar(
+        title: Text('미세먼지앱'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '현재 위치 미세먼지',
+              style: TextStyle(fontSize: 30),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('얼굴사진'),
+                      Text('80'),
+                      Text('보통'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text('얼굴사진'),
+                      Text('80'),
+                      Text('보통'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(Icons.refresh),
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            //side: BorderSide(color: Colors.red)
+                        )
+                    )
+               )
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('현재 위치 미세먼지', style: TextStyle(fontSize: 25),),
-            ],
-          ),
-        ),
+      ),
     );
   }
 }
